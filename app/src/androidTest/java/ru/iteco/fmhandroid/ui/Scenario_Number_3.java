277658ccdbe32df1f087994985b13e20502f5405
buuba <buba86@ro.ru>
2024-12-04 22:42:46 +0500

    Scenario Number 3 PASSED

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/Scenario_Number_3.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/Scenario_Number_3.java
@@ -4,8 +4,11 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -13,6 +16,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static ru.iteco.fmhandroid.ui.customFile.TestUtilities.waitDisplayed;
 
+import android.os.SystemClock;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -50,12 +58,24 @@ public class Scenario_Number_3 {
                 .perform(replaceText(""));
         onView(withId(R.id.news_item_title_text_input_edit_text)).
                 perform(click(), typeText("New News"));
-
-        onView(isRoot()).perform(waitDisplayed(R.id.news_item_create_date_text_input_layout, 5000));
+        // Выбор даты в календаре на текущую
+        onView(isRoot()).perform(waitDisplayed(R.id.news_item_create_date_text_input_layout, 3000));
         onView(withId(R.id.news_item_create_date_text_input_layout)).perform(click());
-        //onView(isRoot()).perform(waitDisplayed(R.id., 5000));
-        //onView(withText("1")).perform(click());
-
-
+        onView(allOf(withText("OK"), isDescendantOfA(withId(android.R.id.content)))).perform(click());
+        // Выбор времени в календаре на текущее
+        onView(isRoot()).perform(waitDisplayed(R.id.news_item_publish_time_text_input_layout, 3000));
+        onView(withId(R.id.news_item_publish_time_text_input_layout)).perform(click());
+        onView(allOf(withText("OK"), isDescendantOfA(withId(android.R.id.content)))).perform(click());
+        // Ввод текста в поле "Description"
+        onView(isRoot()).perform(waitDisplayed(R.id.news_item_description_text_input_layout, 3000));
+        onView(withId(R.id.news_item_description_text_input_edit_text)).perform(click(), typeText("Checking text input in English"));
+        // Создание новости кликом по кнопке "Save"
+        onView(withId(R.id.save_button)).perform(click());
+        // Проверка отображения созданной новости в списке
+        onView(isRoot()).perform(waitDisplayed(R.id.news_list_recycler_view, 5000));
+        onView(withId(R.id.news_list_recycler_view))
+                .perform(RecyclerViewActions.scrollTo(hasDescendant(withText("New News"))));
+        onView(withText("New News"))
+                .check(matches(isDisplayed()));
     }
 }
